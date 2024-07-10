e66de7b087cb85a9a3c838a16bb562213b496d9d
Jeffer Andres Tafurth Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2024-07-10 11:03:06 -0500

    Bootcamp Desarrollo Web Full Stack

---
+++ parte-1-introduccion-react/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css'
+
+function Hello(){
+  return(
+    <>
+      <h2>Hello World</h2>
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello />
+    </>
+  )
+}
+
+export default App
